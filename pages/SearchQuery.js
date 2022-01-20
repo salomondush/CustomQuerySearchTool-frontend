31ff955256bea9956bd563fdfb08b77ffc9d6cbf
salomondush <63796975+salomondush@users.noreply.github.com>
2022-01-19 23:13:20 -0600

    inital setup #1

---
+++ pages/SearchQuery.js
@@ -0,0 +1,7 @@
+export default function SearchQeury(props){
+    return (
+        <div className="search-query">
+            to be implemented
+        </div>
+    )
+}
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import SearchQeury from '../pages/SearchQuery';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        <SearchQeury />
+      </body>
+    </div>
+  );
+}
+
+export default App;
